b7f8cb20ded3a49ccfcfa95f55ce2daff7a420b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-21 19:02:07 +0000

    Deploy 4040E039/weather to github.com/4040E039/weather.git:gh-pages

---
+++ precache-manifest.6b0d48dfd7573b4dc5e35f81dc743903.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5a24669ab0bd05b388",
+    "url": "css/app.d4e06d39.css"
+  },
+  {
+    "revision": "37fc0575ad0e3833a109",
+    "url": "css/chunk-vendors.084c0951.css"
+  },
+  {
+    "revision": "7c80b2103472f475a153839a07c96fb4",
+    "url": "image/w1.png"
+  },
+  {
+    "revision": "7c80b2103472f475a153839a07c96fb4",
+    "url": "image/w2.png"
+  },
+  {
+    "revision": "2acbd686c3205fdc0a8d509886f91a6d",
+    "url": "image/w4.png"
+  },
+  {
+    "revision": "3102988b301d59c2756f54d90b301185",
+    "url": "image/w5.png"
+  },
+  {
+    "revision": "c0d73f101b45cb2a4622cd9e8d1ab9cc",
+    "url": "image/w6.png"
+  },
+  {
+    "revision": "0ab1ae0fc22c119ba95d146ad36d721b",
+    "url": "image/w7.png"
+  },
+  {
+    "revision": "d3ac0e690a16760bdcc9fe274e98b64b",
+    "url": "image/w8.png"
+  },
+  {
+    "revision": "d51479c7ee847554984ea9a4f6b207c9",
+    "url": "image/w9.png"
+  },
+  {
+    "revision": "76efbc8c082d198141af2514ead48e9e",
+    "url": "index.html"
+  },
+  {
+    "revision": "7d5a24669ab0bd05b388",
+    "url": "js/app.17f5af1e.js"
+  },
+  {
+    "revision": "37fc0575ad0e3833a109",
+    "url": "js/chunk-vendors.dfed84b0.js"
+  },
+  {
+    "revision": "2d25a291f63207ad0cc6ccd79e3d0eab",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
